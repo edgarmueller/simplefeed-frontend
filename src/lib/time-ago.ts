1a1e2caee14b0dabebce66056275552bb83ca4c6
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-04-30 10:09:30 +0200

    chore: tz conversion with timeago

---
+++ src/lib/time-ago.ts
@@ -0,0 +1,19 @@
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+
+
+function getLang() {
+	console.log(navigator.languages)
+  if (navigator.languages !== undefined) 
+    return navigator.languages[navigator.languages.length - 1]; 
+  return navigator.language;
+}
+
+TimeAgo.addDefaultLocale(en);
+const timeAgo = new TimeAgo(getLang());
+
+export const formatTimeAgo = (dateString: string) => {
+	// timeago doesn't seem to consider timezone (?)
+	const date = new Date(new Date(dateString).getTime() - new Date().getTimezoneOffset() * 60 * 1000);
+	return timeAgo.format(date);
+}
