1f3acf22e406e0e7153f1bfb4db22e6860295340
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-05-23 23:42:56 +0200

    fix: hasMore prop in `PostList`

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -12,40 +12,38 @@ export interface PostListProps {
 
 export const PostList = ({ userId }: PostListProps) => {
   const [pageNumber, setPageNumber] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
+  const [hasMore, setHasMore] = useState(false);
   const [posts, setPosts] = useState<PostEntity[]>([]);
   useEffect(() => {
     if (!userId) {
       return;
     }
     // TODO: can we fetch by different means
-    // fetchLikedPosts();
     fetchPosts(userId, pageNumber).then((page) => {
       setPosts((p) => uniqBy([...p, ...page.items], "id"));
-      setHasMore(page.meta.totalPages !== pageNumber);
+      setHasMore(
+        page.meta.totalPages !== 0 && page.meta.totalPages !== pageNumber
+      );
     });
   }, [userId, pageNumber]);
   const memoedPosts = useMemo(
     () => posts.map((post) => <Post key={post.id} post={post} />),
     [posts]
   );
-  console.log({posts})
   return (
-    <>
-      <InfiniteScroll
-        key={userId}
-        dataLength={posts.length}
-        next={() => setPageNumber(pageNumber + 1)}
-        hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-      >
-        {memoedPosts}
-      </InfiniteScroll>
-    </>
+    <InfiniteScroll
+      key={userId}
+      dataLength={posts.length}
+      next={() => setPageNumber(pageNumber + 1)}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      {memoedPosts}
+    </InfiniteScroll>
   );
 };
