047bca338ad1c9cbdc1b67a66df50be163ac33c5
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-06-09 12:00:13 +0200

    fix: clear submit form

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -5,12 +5,13 @@ import { Post } from '../domain.interface';
 import { useUser } from '../lib/auth/hooks/useUser';
 
 export const SubmitForm = ({ onSubmit, postTo }: { onSubmit: (post: Post) => void, postTo?: string }) => {
-  const [text, setPostContent] = useState('');
+  const [text, setText] = useState('');
   const { user } = useUser();
 	const handleSubmit = async (event: any) => {
     event.preventDefault();
     const post = await submitPost(text, postTo ? postTo : user?.id)
     onSubmit(post);
+    setText("");
 	};
   return (
     <Stack direction="row" marginBottom={4}>
@@ -19,7 +20,7 @@ export const SubmitForm = ({ onSubmit, postTo }: { onSubmit: (post: Post) => voi
         name="postContent"
         id="post_text"
         placeholder="What's on your mind?"
-        onChange={(e) => setPostContent(e.target.value)}
+        onChange={(e) => setText(e.target.value)}
       />
       <Button onClick={handleSubmit} color='black' id="post_button">Post</Button>
     </Stack>
