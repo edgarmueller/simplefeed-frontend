0c1fe77c8f953b969d173ca7028a0fe3b326603c
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-07-05 20:46:23 +0200

    fix: call onScrollToBottom before scollbar

--- src/components/chat/ScrollableBox.tsx
+++ src/components/chat/ScrollableBox.tsx
@@ -1,16 +1,34 @@
 import { Box } from "@chakra-ui/react";
 import { useEffect, useRef } from "react";
 
-export function ScrollableBox({ children, ...boxProps }: any) {
+export function ScrollableBox({ children, onScrollToBottom, ...boxProps }: any) {
   const boxRef = useRef<any>(null);
 
+  const handleScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = boxRef.current;
+ 
+    if (scrollTop + clientHeight === scrollHeight) {
+      // Scrolled to the bottom
+      if (typeof onScrollToBottom === 'function') {
+        onScrollToBottom();
+      }
+    } 
+  };
+
   useEffect(() => {
-    // Automatically scroll to the bottom when new content is added
+    // TODO: Automatically scroll to the bottom when new content is added
+    // keep enabled?
     boxRef.current.scrollTop = boxRef.current.scrollHeight;
   }, [children]);
 
+  useEffect(() => {
+    if (boxRef.current.scrollHeight <= boxRef.current.clientHeight) {
+      onScrollToBottom();
+    }
+  }, []);
+
   return (
-    <Box overflow="auto" ref={boxRef} {...boxProps}>
+    <Box overflow="auto" onScroll={handleScroll} ref={boxRef} {...boxProps}>
       {children}
     </Box>
   );
