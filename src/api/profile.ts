2611a0b39301291707d1fcc813f22ba2a6e15361
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-09-25 22:52:41 +0200

    chore: add missing auth when fetching user profile

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -5,6 +5,7 @@ export async function fetchUserProfile(username: string): Promise<any> {
   const res = await axios.get(`${API_URL}/users/${username}`, {
     headers: {
       "Content-Type": "application/json",
+      ...createHeaders(),
     },
   });
   return res.data;
