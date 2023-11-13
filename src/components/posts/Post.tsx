296056b1a796ef0b6abeb475a2119262ca98c5a6
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-11-14 00:19:41 +0100

    refactor: extract header

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -24,6 +24,45 @@ import { formatTimeAgo } from "../../lib/time-ago";
 import { Comments } from "./Comments";
 import { LikeButton } from "./LikeButton";
 
+type PostHeaderProps = {
+  author: {
+    username: string;
+    imageUrl: string;
+    firstName: string;
+    lastName: string;
+  }
+  postedTo?: {
+    username: string;
+    firstName: string;
+    lastName: string;
+  }
+  createdAt: string;
+}
+
+function PostHeader({ createdAt, author, postedTo }: PostHeaderProps) {
+  return (
+    <Stack direction="row" spacing={4} alignItems="center" marginBottom={2}>
+      <Avatar
+        size="sm"
+        name={author.username}
+        src={author.imageUrl}
+      />
+      <div className="posted_by" style={{ color: "#ACACAC" }}>
+        <Link as={RouterLink} to={`/users/${author.username}`}>
+          {author.firstName} {author.lastName}
+        </Link>{" "}
+        {postedTo && "to "}
+        {postedTo && (
+          <Link as={RouterLink} to={`/users/${postedTo.username}`}>
+            {postedTo.firstName} {postedTo.lastName}
+          </Link>
+        )}
+      </div>
+      <Box marginBottom={4}>{formatTimeAgo(createdAt)}</Box>
+    </Stack>
+  )
+}
+
 const Post = memo(({ post }: { post: PostEntity }) => {
   const { user, decrementPostCount } = useUser();
   const [comments, setComments] = useState<Pagination<Comment>>({
@@ -62,27 +101,10 @@ const Post = memo(({ post }: { post: PostEntity }) => {
   const onReply = useCallback((comment: Comment) => {
     setComments((prevComments) => ({ ...prevComments, items: [...(prevComments.items || []), comment] }));
 }, []);
+console.log({ post })
 return isVisible ? (
   <Box key={post.id} bg="gray.100" p={3} borderRadius="md" marginBottom={3}>
-    <Stack direction="row" spacing={4} alignItems="center" marginBottom={2}>
-      <Avatar
-        size="sm"
-        name={post.author.username}
-        src={post.author.imageUrl}
-      />
-      <div className="posted_by" style={{ color: "#ACACAC" }}>
-        <Link as={RouterLink} to={`/users/${post.author.username}`}>
-          {post.author.firstName} {post.author.lastName}
-        </Link>{" "}
-        {post.postedTo && "to "}
-        {post.postedTo && (
-          <Link as={RouterLink} to={`/users/${post.postedTo.username}`}>
-            {post.postedTo.firstName} {post.postedTo.lastName}
-          </Link>
-        )}
-      </div>
-      <Box marginBottom={4}>{formatTimeAgo(post.createdAt)}</Box>
-    </Stack>
+    <PostHeader author={post.author} postedTo={post.postedTo} createdAt={post.createdAt} />
     <Box bg="white" padding={2} borderRadius="md" marginBottom={4}>
       <Markdown>{post.body}</Markdown>
       {(post.attachments || [])
