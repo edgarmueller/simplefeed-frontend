6d6ae2f3b3f515461fc19b526d3778f1bdf671e9
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-11-13 18:38:19 +0100

    chore: escape comment paths

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -88,12 +88,20 @@ export interface CommentNode {
   children: CommentNode[];
 }
 
+function escape(path: string) {
+  // console.log({ path })
+  const ret = path.replace(/-/g, "_").replace(/\//gi, '.');
+  // console.log( { ret })
+  return ret
+}
+
 export function buildCommentTree(comments: Comment[]): CommentNode[] {
   const commentMap = new Map<string, CommentNode>();
 
+
   // First pass: create a CommentNode for each comment
   for (const comment of comments) {
-    commentMap.set(comment.id, {
+    commentMap.set(escape(comment.id), {
       comment,
       children: [],
     });
@@ -101,32 +109,33 @@ export function buildCommentTree(comments: Comment[]): CommentNode[] {
 
   // Second pass: link CommentNodes based on their paths
   for (const comment of comments) {
-    const path = comment.path?.split("/");
+    const path = escape(comment.path).split(".");
     if (path.length > 1) {
-      const parentId = path[path.length - 1];
+      const parentId = escape(path[path.length - 1]);
       const parent = commentMap.get(parentId);
       if (parent) {
-        parent.children.push(commentMap.get(comment.id)!);
+        parent.children.push(commentMap.get(escape(comment.id))!);
       }
     }
   }
 
   // Return the root nodes (comments with no parents)
   return Array.from(commentMap.values()).filter((node) => {
-    const path = node.comment.path?.split("/");
+    const path = node.comment.path?.split(".");
     return path.length === 1;
   });
 }
 
+export const COMMENTS_FETCH_LIMIT = 1;
 export async function fetchComments(
   postId: string,
   page = 1,
   commentId = postId
 ): Promise<Pagination<Comment>> {
   // TODO: hard coded limit
-  const limit = 50;
+  const limit = COMMENTS_FETCH_LIMIT;
   const url = commentId
-    ? `${API_URL}/posts/${postId}/comments/${commentId}?page=${page}&limit=${limit}}`
+    ? `${API_URL}/posts/${postId}/comments/${commentId}?page=${page}&limit=${limit}`
     : `${API_URL}/posts/${postId}/comments?page=${page}&limit=${limit}`;
   const res = await axios.get(url, {
     headers: createHeaders(),
