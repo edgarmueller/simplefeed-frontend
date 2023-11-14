159f4058f2e1596ed2f84bf8aa7902fb45ea72ab
Edgar Mueller <edgar.m.mueller@gmail.com>
2023-11-14 09:51:10 +0100

    refactor: use loader for loading posts

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,28 +1,36 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, defer, Await } from "react-router-dom";
 import { fetchPost } from "../api/posts";
 import { Layout } from "../components/common/Layout";
 import { Post as PostEntity } from "../domain.interface";
 import { Post } from "../components/posts/Post";
+import React from "react";
+
+export async function loader({ params }: any) {
+	try {
+		return defer({ post: fetchPost(params.postId) })
+	} catch (error) {
+		console.error(error)
+	}
+}
 
 const PostPage = () => {
-	const { id: postId } = useParams();
-	const [post, setPost] = useState<PostEntity>()
-  useEffect(() => {
-		if (!postId) {
-			return;
-		}
-		fetchPost(postId).then((post) => {
-			setPost(post)
-		});
-	}, []);
-  return (
-    <Layout>
-			{
-				post ? <Post post={post} /> : null
-			}
-    </Layout>
-  );
+	const data = useLoaderData() as { post: PostEntity };
+	return (
+		<Layout>
+			<React.Suspense
+				fallback={<p>Loading post...</p>}
+			>
+				<Await
+					resolve={data.post}
+					errorElement={
+						<p>Error loading post!</p>
+					}
+				>
+					{(post) => <Post post={post} />}
+				</Await>
+			</React.Suspense>
+		</Layout>
+	);
 };
 
 export default PostPage;
