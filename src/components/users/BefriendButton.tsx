b6c1dfc04e6bae9a0e1689fa5e28b5eb252d7569
Edgar Mueller <edgar.m.mueller@gmail.com>
2024-08-06 23:23:48 +0200

    feat: add toast for `BefriendButton`

--- src/components/users/BefriendButton.tsx
+++ src/components/users/BefriendButton.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
-import { makeFriendRequest as makeFriendRequestApi } from "../../api/friends";
+import { makeFriendRequest } from "../../api/friends";
 
 export interface BefriendButtonProps {
   username: string;
@@ -12,30 +12,42 @@ export const BefriendButton = ({
   username,
   hasFriendRequest,
 }: BefriendButtonProps) => {
-	const [friendRequestSent, setFriendRequestSent] = useState<boolean>(hasFriendRequest);
-  const makeFriendRequest = useMutation({
-    mutationFn: (username: string) => makeFriendRequestApi(username)
-  }) 
-
-  if (hasFriendRequest || friendRequestSent) {
-    return (
-      <Button variant="outline" size="sm" isDisabled>
-        Friend request sent
-      </Button>
-    );
-  }
+  const toast = useToast();
+  const [friendRequestSent, setFriendRequestSent] = useState<boolean>(hasFriendRequest);
+  const makeFriendRequestMutation = useMutation({
+    mutationFn: (username: string) => makeFriendRequest(username),
+    onSuccess: () => {
+      setFriendRequestSent(true);
+      toast({
+        title: "Friend request sent",
+        status: "success",
+        duration: 5000,
+        isClosable: true
+      })
+    },
+    onError: (error: Error) => {
+      toast({
+        title: `Friend request failed: ${error.message}. Please try again later.`,
+        status: "error",
+        duration: 5000,
+        isClosable: true
+      })
+    }
+  })
+  const friendRequestExists = hasFriendRequest || friendRequestSent;
 
   return (
-    <>
-      <Button
-        variant="outline"
-        size="sm"
-        onClick={async () => {
-					await makeFriendRequest.mutate(username)
-					setFriendRequestSent(true)
-				}}>
-        Befriend
-      </Button>
-    </>
+    <Button
+      variant="outline"
+      size="sm"
+      onClick={() => {
+        makeFriendRequestMutation.mutate(username)
+      }}
+      isDisabled={makeFriendRequestMutation.isPending || friendRequestExists}
+    >
+      {
+        friendRequestExists ? "Friend request sent" : "Befriend"
+      }
+    </Button>
   );
 };
